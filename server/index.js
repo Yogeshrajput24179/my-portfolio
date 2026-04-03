23625ee3da5a20ccb6353e3952f728f25d1e66cb
YOGESH <111962172+Yogeshrajput24179@users.noreply.github.com>
2026-04-03 12:29:40 +0530

    change client url

--- server/index.js
+++ server/index.js
@@ -11,7 +11,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
